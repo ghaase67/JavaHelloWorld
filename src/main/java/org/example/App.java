785237fd2e787a7675ae889d6de57116a5f701dc
Guido Haase <guido.haase@gmx.net>
2021-08-11 14:56:44 +0200

    test 4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,6 @@ package org.example;
 public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World 3!" );
+        System.out.println( "Hello World 4!" );
     }
 }
